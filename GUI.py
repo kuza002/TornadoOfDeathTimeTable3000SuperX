1a0d1e0fc200b457d0f9b0a6bb355c37abc47a80
DOS <kiler027@gmail.com>
2022-02-22 14:10:00 +0300

    It's Alive

--- GUI.py
+++ GUI.py
@@ -5,9 +5,8 @@ import shutil
 import threading
 import tkinter as tk
 import openpyxl
-from tkinter import Tk, BOTH, filedialog, PhotoImage,Menu
+from tkinter import Tk, BOTH, filedialog, PhotoImage, Menu, messagebox as mbox
 from tkinter.ttk import Frame, Button, Style, Entry, Label
-from tkinter import messagebox as mbox
 from Settings_window import Worker_set
 from parse_time_table import Parser
 from support_file import *
@@ -35,27 +34,26 @@ class Example(Frame):
         self.style.theme_use("default")
         self.pack(fill=BOTH, expand=1)
 
-        menubar = Menu(self.master)
+        menubar = Menu(self)
+        menubar.add_cascade(label="Сотрудники", command=self.open_worker_settings)
         self.parent.config(menu=menubar)
-        fileMenu = Menu(menubar)
-        fileMenu.add_command(label="Сотрудники", command=self.open_worker_settings)
-        menubar.add_cascade(label="*****", menu=fileMenu)
-        bies = 100
+
+        bias = 100
 
         file_label = Label(self, text='Расположение файла с расписания занятий')
-        file_label.place(x=20 + bies, y=70)
+        file_label.place(x=20 + bias, y=70)
 
         self.file_path = Entry(self)
-        self.file_path.place(x=20 + bies, y=90, width=267, height=30)
+        self.file_path.place(x=20 + bias, y=90, width=267, height=30)
 
         file_picker = Button(self, text="Выбрать файл", command=self.open_file)
-        file_picker.place(x=20 + bies, y=130)
+        file_picker.place(x=20 + bias, y=130)
 
         self.parse_button = Button(self, text="Спарсить", command=self.parse_file)
-        self.parse_button.place(x=200 + bies, y=130)
+        self.parse_button.place(x=200 + bias, y=130)
 
         just_button = Button(self, text='Работать!', command=self.make_table)
-        just_button.place(x=120 + bies, y=450)
+        just_button.place(x=120 + bias, y=450)
 
     def open_file(self):
         ftypes = [('Excel files', '*.xlsx')]
@@ -66,8 +64,9 @@ class Example(Frame):
 
     def open_worker_settings(self):
         window = Worker_set(self)
-        window.geometry("512x512")
+        window.geometry("300x256")
         window.grab_set()
+
     @thread
     def parse_file(self):
         self.parse_button.config(state=tk.DISABLED)
@@ -84,9 +83,11 @@ class Example(Frame):
 
     def make_table(self):
         if not self.parsed:
-            with open('some_files/data.pickle', 'rb') as f:
-                self.data = pickle.load(f)
-
+            if os.path.exists('some_files/data.pickle'):
+                with open('some_files/data.pickle', 'rb') as f:
+                    self.data = pickle.load(f)
+            else:
+                mbox.showerror('Ошибка!', 'Не найден файл data.pickle')
         wb = openpyxl.load_workbook('some_files/template.xlsx')
 
         groups = ['Илья Г.:09-933 (1)',
@@ -94,23 +95,23 @@ class Example(Frame):
                   'Ахад:09-063 (1)',
                   'Максим:09-012 (1)',
                   'Илья К.:09-012 (1)', ]
-
-
+        with open('some_files/workers.pickle', 'rb') as f:
+            workers = pickle.load(f)
         # Create area to put lessons ( sry about this ;) )
         magic_var = list(range(6, 77, 14))
 
         # Getting lessons for all groups
         all_lessons = {}
 
-        for group in groups:
-            lessons_for_group = self.data.get_lessons_by_group(group.split(':')[1])
-            all_lessons[group] = lessons_for_group
+        for worker in workers:
+            all_lessons[':'.join(worker)] = self.data.get_lessons_by_group(worker[1]+worker[2])
 
         # region Create table for workers
         group_coord = 'C'
-        for group, lessons in all_lessons.items():
-            wb[wb.sheetnames[1]][group_coord + "1"] = group.split(':')[0]
-            wb[wb.sheetnames[1]][group_coord + "2"] = group.split(':')[1]
+        for worker, lessons in all_lessons.items():
+            worker=worker.split(':')
+            wb[wb.sheetnames[1]][group_coord + "1"] = worker[0]
+            wb[wb.sheetnames[1]][group_coord + "2"] = worker[1]+worker[2]
             paint_cells(wb[wb.sheetnames[1]], lessons, bad_color, magic_var, group_coord, True)
 
             group_coord = chr(ord(group_coord) + 1)
--- Settings_window.py
+++ Settings_window.py
@@ -1,31 +1,70 @@
 import os
 import pickle
-from tkinter import Toplevel, Listbox, END
+import re
+from tkinter import Toplevel, Listbox, END, messagebox as mbox
 from tkinter.ttk import Button, Entry
 
 
 class Worker_set(Toplevel):
+    index=0
     def __init__(self, parent):
         super().__init__(parent)
+        self.initUI()
 
+
+    def initUI(self):
         self.lb = Listbox(self)
         if os.path.exists('some_files/workers.pickle'):
             with open('some_files/workers.pickle', 'rb') as f:
                 self.workers = pickle.load(f)
                 for i in self.workers:
-                    self.lb.insert(END, i.split(':')[0])
+                    self.lb.insert(self.index, i[0])
+                    self.index += 1
+        else:
+            self.workers = []
 
         self.lb.place(x=0, y=0)
         self.lb.bind("<<ListboxSelect>>", self.onSelect)
+        self.entry_list = []
+        for i in range(2):
+            self.entry_list.append(Entry(self))
+            self.entry_list[-1].place(x=150, y=30 * (i + 1))
+        Button(self, text='Сохранить', command=self.save_data).place(x=150, y=30 * 3)
         self.e_add = Entry(self)
         self.e_add.place(x=0, y=180)
         Button(self, text="Добавить сотрудника", command=self.addItem).place(x=0, y=200)
 
     def addItem(self):
-        self.lb.insert(END, self.e_add.get())
-        self.workers.append(f'{self.e_add.get()}:')
+        self.lb.insert(self.index, self.e_add.get())
+        self.index+=1
+        self.workers.append([self.e_add.get(),'',''])
 
 
-    def onSelect(self):
-        pass
+    def onSelect(self,event):
+        worker=self.workers[event.widget.curselection()[0]][1:]
+        print(event.widget.curselection()[0])
+        for i in range(len(self.entry_list)):
+            self.entry_list[i].delete(0,END)
+            self.entry_list[i].insert(0,worker[i])
+
+
+    def save_data(self):
+        pattern_group=r'^09-[0-9][0-6][1-5]$'
+        pattern_subgroup = r'^\([1-2]\)$'
+        group=self.entry_list[0].get()
+        subgroup=self.entry_list[1].get()
+        if re.search(pattern_group,group.strip()):
+            self.workers[self.lb.curselection()[0]][1]=f'{group} '
+            print(self.workers[self.lb.curselection()[0]])
+        else:
+            mbox.showerror('Ошибка!', 'Некорректные данные!')
+        if re.search(pattern_subgroup,subgroup.strip()):
+            self.workers[self.lb.curselection()[0]][2] = subgroup
+            print(self.workers[self.lb.curselection()[0]])
+        else:
+            mbox.showwarning('Предупреждение','Не указана подгруппа!\n Автоматический будет установлена первая')
+            self.workers[self.lb.curselection()[0]][2] = '(1)'
+        with open('some_files/workers.pickle', 'wb') as f:
+            pickle.dump(self.workers, f)
+
 
